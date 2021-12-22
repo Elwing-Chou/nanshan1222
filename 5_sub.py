f0fc84f941763ed79cc64a8f97cde12cd25a18e9
Elwing-Chou <birdfan8814@gmail.com>
2021-12-22 16:29:04 +0800

    Add files via upload

---
+++ 5_sub.py
@@ -0,0 +1,14 @@
+# MAC/LINUX: ls -al
+# Windows: dir(只有從終端機才看得到)
+# shell=True: 字串
+# shell=False: list, 第一個位置會是執行檔, 後面位置被當成參數
+# ["ls", "-al", "rm", "-rf"], rm會被當成參數, 完全不執行
+import subprocess
+
+# subprocess.run("ls -al;rm -rf", shell=True)
+# WIN
+# subprocess.run("dir", shell=True)
+# f = open("log.txt", "w")
+with open("log.txt", "w") as f, open("error.txt", "w") as e:
+    result = subprocess.run(["ls", "-al"], stdout=f, stderr=e)
+print(result)
