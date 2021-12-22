2212d896d1e5c688cef9e21fd0e0515997a08302
Elwing-Chou <birdfan8814@gmail.com>
2021-12-22 14:30:55 +0800

    Add files via upload

--- 1_oo.py
+++ 1_oo.py
@@ -3,11 +3,15 @@
 # 物件導向(自訂型態)
 # 自訂型態: 值/函式
 class Person:
+
+    def __init__(self, w, h):
+        self.height = h
+        self.weight = w
+
     def cbmi(self):
         return self.weight / (self.height / 100) ** 2
 
-
-p1 = Person()
-p1.height = 175
-p1.weight = 75
+p1 = Person(75, 174)
 print(p1.cbmi())
+p2 = Person(80, 180)
+print(p2.cbmi())
