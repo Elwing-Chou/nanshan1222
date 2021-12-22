6da32529508f393778685d117c0a462ef3dd2fbc
Elwing-Chou <birdfan8814@gmail.com>
2021-12-22 16:37:55 +0800

    Add files via upload

--- 5_sub.py
+++ 5_sub.py
@@ -6,9 +6,17 @@
 import subprocess
 
 # subprocess.run("ls -al;rm -rf", shell=True)
-# WIN
+# WIN:
 # subprocess.run("dir", shell=True)
 # f = open("log.txt", "w")
 with open("log.txt", "w") as f, open("error.txt", "w") as e:
     result = subprocess.run(["ls", "-al"], stdout=f, stderr=e)
+print(result)
+
+with open("log2.txt", "w") as f, open("error2.txt", "w") as e:
+    result = subprocess.run(["pip", "install", "jieba"], stdout=f, stderr=e)
+print(result)
+
+with open("log3.txt", "w") as f, open("error3.txt", "w") as e:
+    result = subprocess.run(["python", "4_gui.py"], stdout=f, stderr=e)
 print(result)
