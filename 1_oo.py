181bc665f1ad998eb105497cea51c19f19d00c22
Elwing-Chou <birdfan8814@gmail.com>
2021-12-22 14:48:23 +0800

    Add files via upload

--- 1_oo.py
+++ 1_oo.py
@@ -11,7 +11,18 @@ class Person:
     def cbmi(self):
         return self.weight / (self.height / 100) ** 2
 
+    def __str__(self):
+        return "{}/{}".format(self.weight,
+                              self.height)
+
+    def __eq__(self, other):
+        return self.weight == other.weight
+
 p1 = Person(75, 174)
+# str(p1) -> p1.__str__()
+print(p1)
 print(p1.cbmi())
-p2 = Person(80, 180)
+p2 = Person(75, 180)
 print(p2.cbmi())
+# p1.__eq__(p2)
+print(p1 == p2)
